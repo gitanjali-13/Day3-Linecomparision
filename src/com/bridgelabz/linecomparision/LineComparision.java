df5d4d8c199ea98823e90dea4ba569e2b65c9fc1
gitanjali-13 <gitaanjalide@gmail.com>
2022-09-13 13:49:26 +0530

    Line comparision problem

---
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.linecomparision;
+
+import java.util.Scanner;
+
+public class LineComparision {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Line Comparison Computation Program ");
+
+
+    }
+}
